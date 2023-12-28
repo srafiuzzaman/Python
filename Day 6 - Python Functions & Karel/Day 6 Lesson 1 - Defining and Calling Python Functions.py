b76d04b22881bac36e6a9f40a05b1d95aa59190e
srafiuzzaman <srafiuzzaman9763@students.mc3.edu>
2023-12-28 07:40:36 -0500

    Day 6 Assignments Completed

--- Day 6 - Python Functions & Karel/Day 6 Lesson 1 - Defining and Calling Python Functions.py
+++ Day 6 - Python Functions & Karel/Day 6 Lesson 1 - Defining and Calling Python Functions.py
@@ -1,2 +1,17 @@
 # Sahad Rafiuzzaman
 # 12/28/2023
+
+# print function
+print ("Hello")
+
+# len function
+num_char = len("Hello")
+print(num_char)
+
+# defining a custom function
+def my_function():
+    print("Hello")
+    print("Bye")
+
+# Calling the function
+my_function()
---
+++ Day 6 - Python Functions & Karel/Day 6 Lesson 2 - While Loop.py
@@ -0,0 +1,19 @@
+# Sahad Rafiuzzaman
+# 12/28/2023
+# While Loops
+
+number_of_hurdles = 6
+while number_of_hurdles > 0:
+    print("Jump")
+    number_of_hurdles -= 1
+    print(number_of_hurdles)
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+import random
+
+
+at_goal = random.randint(1,10)
+while not at_goal == 0:
+    at_goal -= 1
+    print("Jump")
+
